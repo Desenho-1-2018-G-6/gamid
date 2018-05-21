f271ebbda179c0f9c62c270624b476da1f2486d1
Lucas Malta <lucas50xd@hotmail.com>
2018-05-21 09:58:24 -0300

    makes time object oriented

--- src/time.js
+++ src/time.js
@@ -1,41 +1,45 @@
-let time = (function(){
-        let lastUpdate = 0;
-        let setTime = function(){
-            lastUpdate = Date.now();  
-            setInterval(loop, 16.6667);
+let time = (function () {
+    class Time {
+        constructor() {
+            this.lastUpdate = 0;
+            this.objects = gamid.graphics.objects;
+            this.canvasList = gamid.graphics.canvasList;
         }
 
-        
-        function loop(){
+        setTime() {
+            let that = this;
+            this.lastUpdate = Date.now();
+            setInterval(() => this.loop(), 16.6667);
+        }
+
+        loop() {
             let now = Date.now();
-            let dt = now - lastUpdate;
-            lastUpdate = now;
-            updateGame(dt);
+            let dt = now - this.lastUpdate;
+            this.lastUpdate = now;
+            this.updateGame(dt);
         }
 
-        function updateGame(dt){
-            let objects = gamid.graphics.objects;
-            let canvasList = gamid.graphics.canvasList;
-            
-            for(let i in canvasList){
+        updateGame(dt) {
+            for (let i in this.canvasList) {
                 // clear all canvasList
-                let ctx = canvasList[i].getContext('2d');
-                ctx.clearRect(0, 0, canvasList[i].width, canvasList[i].height);
-                gamid.graphics.setBackgroundColor(canvasList[i], canvasList[i].color);
+                let ctx = this.canvasList[i].getContext('2d');
+                ctx.clearRect(0, 0, this.canvasList[i].width, this.canvasList[i].height);
+                gamid.graphics.setBackgroundColor(this.canvasList[i], this.canvasList[i].color);
 
                 // show fps
                 ctx.fillStyle = 'black';
-                ctx.fillText('fps:' + dt + 'ms', 5, 10);
+                ctx.fillText('fps:' + parseFloat(1/(dt/1000)).toFixed(2) + 'ms', 5, 10);
             }
 
             // move all objects
-            for (let i in objects) {
-                objects[i].newPosition(dt/1000); // delta time in seconds
-                objects[i].update();
+            for (let i in this.objects) {
+                this.objects[i].newPosition(dt / 1000); // delta time in seconds
+                this.objects[i].update();
             }
         }
 
-    return {
-        setTime
     }
+
+    new Time().setTime();
+    
 }());
