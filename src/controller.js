d5a58bb977aa2472bc9dff1aa592be724d42d87a
Ateldy Brasil <ateldybfilho@gmail.com>
2018-06-11 10:31:50 -0300

    Moves Keyboard functions to Controller class
    Co-authored-by: Ateldy Brasil <ateldybfilho@gmail.com>
    Co-authored-by: Thallisson Melo <tallisonmelo@hotmail.com>
    Co-authored-by: Lucas Martins <lucas465@hotmail.com>

--- src/controller.js
+++ src/controller.js
@@ -1,102 +1,100 @@
 class BaseObjectDecorator extends graphics.BaseObject {
-  constructor(baseObject){
+  constructor(baseObject) {
     super(baseObject.width, baseObject.height, baseObject.x, baseObject.y);
   }
 
-  onKeyUp(){
+  onKeyUp() {
     super.onKeyUp();
   }
 
-  onKeyDown(){
+  onKeyDown() {
     super.onKeyDown();
   }
 }
 
-let controller = (function(){
+let controller = (function () {
 
   class Controller extends BaseObjectDecorator {
-    constructor(baseObject, keyList){
+    constructor(baseObject, keyList) {
       super(baseObject);
       this.keyList = keyList;
     }
 
-    onKeyUp(){
+    createKeysFunction() {
+      for (let i = 0; i < this.keyList.length; i++) {
+        let obj = new Object();
+        obj.key = this.keyList[i];
+        obj.onKeyUpFunc = function () { };
+        obj.onKeyDownFunc = function () { };
+        this.functionList.push(obj);
+      }
+
+    }
+
+    setOnKeyUp(key, func) {
+      let counter = false;
+      for (let i in this.functionList) {
+        if (key == this.functionList[i].key) {
+          this.functionList[i].onKeyUpFunc = func;
+          counter = true;
+        }
+      }
+      if (!counter) {
+        return "Could not find key";
+      }
+      return "Function changed";
+    }
+
+    setOnKeyDown(key, func) {
+      let counter = false;
+      for (let i in this.functionList) {
+        if (key == this.functionList[i].key) {
+          this.functionList[i].onKeyDownFunc = func;
+          counter = true;
+        }
+      }
+      if (!counter) {
+        return "Could not find key";
+      }
+      return "Function changed";
+    }
+
+    onKeyUp() {
       super.onKeyUp();
       // console.log("WOOOOOOOOOOO");
     }
 
-    onKeyDown(){
+    onKeyDown() {
       super.onKeyDown();
       console.log("WAAAAAAAAAAAAAA");
     }
   }
 
   class Keyboard extends Controller {
-      constructor(baseObject, keyList){
-          super(baseObject, keyList);
-          this.baseObject = baseObject;
-          this.functionList = [];
-          this.createKeysFunction();
-      }
+    constructor(baseObject, keyList) {
+      super(baseObject, keyList);
+      this.baseObject = baseObject;
+      this.functionList = [];
+      this.createKeysFunction();
+      document.addEventListener("keydown", () => this.onKeyDown(event, this.functionList));
+      document.addEventListener("keyup", () => this.onKeyUp(event, this.functionList));
+    }
 
-      createKeysFunction(){
-          for (let i = 0; i < this.keyList.length; i++){
-              let obj = new Object();
-              obj.key = this.keyList[i];
-              obj.onKeyUpFunc = function(){};
-              obj.onKeyDownFunc = function(){};
-              this.functionList.push(obj);
-          }
-
-          document.addEventListener("keydown", () => this.onKeyDown(event, this.functionList));
-          document.addEventListener("keyup", () => this.onKeyUp(event, this.functionList));
-      }
-
-
-      onKeyDown(event, functionList){
-        for(let i in functionList){
-          if(event.keyCode == functionList[i].key){
-              functionList[i].onKeyDownFunc();
-          }
+    onKeyDown(event, functionList) {
+      for (let i in functionList) {
+        if (event.keyCode == functionList[i].key) {
+          functionList[i].onKeyDownFunc();
         }
       }
+    }
 
-      onKeyUp(event, functionList){
-        for(let i in functionList){
-          if(event.keyCode == functionList[i].key){
-              functionList[i].onKeyUpFunc();
-          }
+    onKeyUp(event, functionList) {
+      for (let i in functionList) {
+        if (event.keyCode == functionList[i].key) {
+          functionList[i].onKeyUpFunc();
         }
       }
-
-      setOnKeyUp(key, func){
-        let counter = false;
-        for(let i in this.functionList){
-          if(key == this.functionList[i].key){
-            this.functionList[i].onKeyUpFunc = func;
-            counter = true;
-          }
-        }
-        if(!counter){
-          return "Could not find key";
-        }
-        return "Function changed";
-      }
-
-      setOnKeyDown(key, func){
-        let counter = false;
-        for(let i in this.functionList){
-          if(key == this.functionList[i].key){
-            this.functionList[i].onKeyDownFunc = func;
-            counter = true;
-          }
-        }
-        if(!counter){
-          return "Could not find key";
-        }
-        return "Function changed";
-      }
-      
+    }
   }
 
   return {
