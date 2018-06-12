fef21e409ae68b625677a9cedd7799d44a62426d
ThalissonMelo <tallisonmelo@hotmail.com>
2018-06-12 11:09:39 -0300

    Adding sound folder to gamid
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Thallisson Melo <tallisonmelo@hotmail.com>
    Co-authored-by: Ateldy Brasil <ateldybfilho@gmail.com>

--- src/audio.js
+++ src/audio.js
@@ -7,32 +7,32 @@ let audio = (function () {
       this.soundList = [];
       this.createAudios(fileList);
     }
-  }
 
-  createAudios(fileList){
-    for(let i in fileList){
-      let sound = document.createElement("audio");
-      sound.src = fileList[i];
-      sound.setAttribute("preload", "auto");
-      sound.setAttribute("control", "none");
-      sound.style.display = "none";
-      document.body.appendChild(sound);
-      this.soundList.push(sound);
-    }
-  }
-
-  play(soundName){
-    for(let i in this.soundList){
-      if(this.soundList[i].attributes.src.nodeValue === soundName){
-        this.soundList[i].play();
+    createAudios(fileList){
+      for(let i in fileList){
+        let sound = document.createElement("audio");
+        sound.src = fileList[i];
+        sound.setAttribute("preload", "auto");
+        sound.setAttribute("control", "none");
+        sound.style.display = "none";
+        document.body.appendChild(sound);
+        this.soundList.push(sound);
       }
     }
-  }
 
-  pause(soundName){
-    for(let i in this.soundList){
-      if(this.soundList[i].attributes.src.nodeValue === soundName){
-        this.soundList[i].pause();
+    play(soundName){
+      for(let i in this.soundList){
+        if(this.soundList[i].attributes.src.nodeValue === soundName){
+          this.soundList[i].play();
+        }
+      }
+    }
+
+    pause(soundName){
+      for(let i in this.soundList){
+        if(this.soundList[i].attributes.src.nodeValue === soundName){
+          this.soundList[i].pause();
+        }
       }
     }
   }
