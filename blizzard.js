d98b1cc281840163b2001a38b3407a01127236cb
Lucas Malta <lucas50xd@hotmail.com>
2018-06-17 14:38:54 -0300

    renames blizzard.js to random.js

--- blizzard.js
+++ blizzard.js
@@ -1,58 +1,36 @@
-let canvasInstance = new gamid.graphics.Canvas(800, 800, 'darkgreen');
+let canvasInstance = new gamid.graphics.Canvas(800, 800, 'black');
 let canvas = canvasInstance.element;
 // gamid.graphics.setBackgroundColor(canvas, 'red');
 
-for (let i = 0; i < 100; i++){
-    // let abc=['blue', 'red', 'yellow', 'green', 'gray']
-    let xxx = Math.random()*800
-    let yyy = Math.random()*800
+for (let i = 0; i < 1000; i++){
+    let square = new gamid.graphics.Square(1, 10,  Math.random()*800, Math.random()*800, 1, 'cyan');
 
-    let color = 'red'
-
-    let abc=['#009','yellow','yellow']
-    if (xxx >400) {
-      abc=['white', 'black']
-    }
-    // FIXME DÁ PRA CRIAR UM OBJETO SQUARE SEM UMA DAS POSIÇÕES X E Y (CORRIJAM) (OU É PQ FICA SEM COR, SEI LÁ)
-    // let square = new gamid.graphics.Square(2, 2, Math.random()*800, Math.random()*800, abc[Math.floor(Math.random()*abc.length)]);
-
-    let square = new gamid.graphics.Square(30, 30, xxx, yyy, 1, abc[Math.floor(Math.random()*abc.length)]);
+    let a = new gamid.controller.Keyboard(square, [87, 65, 83, 68]);
     let squarecollision = new gamid.physics.SquareCollision(square);
+    squarecollision.afterCollision = function(collided){
+        collided.width +=0.01;
+        collided.height +=0.01;
+        square.width +=0.01;
+        square.height +=0.01;
+    }
 
-    a = new gamid.controller.Keyboard(square, [87, 65, 83, 68, 73, 74, 75, 76]);
-     // = new gamid.controller.Keyboard(square, [73, 74, 75, 76]);
+canvasInstance.addObject(square);
+// square.speedX = 20;
+// square.speedY = 20;
 
-    canvasInstance.addObject(square);
-    // square.speedX = 20;
-    // square.speedY = 20;
+// a.setOnKeyUp(87, function(){square.speedY = 0});
+a.setOnKeyDown(87, function(){square.speedY -= Math.random()*100;});
 
-    // a.setOnKeyUp(87, function(){square.speedY = 0});
-    if (xxx<400) {
-      a.setOnKeyDown(87, function(){square.speedY -= Math.random()*100;});
+// a.setOnKeyUp(83, function(){square.speedY = 0});
+a.setOnKeyDown(83, function(){square.speedY += Math.random()*100; });
 
-      // a.setOnKeyUp(83, function(){square.speedY = 0});
-      a.setOnKeyDown(83, function(){square.speedY += Math.random()*100; });
+// a.setOnKeyUp(65, function(){square.speedX = 0});
+a.setOnKeyDown(65, function(){square.speedX -= Math.random()*100; });
 
-      // a.setOnKeyUp(65, function(){square.speedX = 0});
-      a.setOnKeyDown(65, function(){square.speedX -= Math.random()*100; });
-
-      // a.setOnKeyUp(68, function(){square.speedX = 0});
-      a.setOnKeyDown(68, function(){square.speedX += Math.random()*100; });
-    } else {
-      a.setOnKeyDown(73, function(){square.speedY -= Math.random()*100;});
-
-      // a.setOnKeyUp(83, function(){square.speedY = 0});
-      a.setOnKeyDown(75, function(){square.speedY += Math.random()*100; });
-
-      // a.setOnKeyUp(65, function(){square.speedX = 0});
-      a.setOnKeyDown(74, function(){square.speedX -= Math.random()*100; });
-
-      // a.setOnKeyUp(68, function(){square.speedX = 0});
-      a.setOnKeyDown(76, function(){square.speedX += Math.random()*100; });
-  }
+// a.setOnKeyUp(68, function(){square.speedX = 0});
+a.setOnKeyDown(68, function(){square.speedX += Math.random()*100; });
 
 }
-
 document.body.appendChild(canvas);
 
 
---
+++ random.js
@@ -0,0 +1,66 @@
+let canvasInstance = new gamid.graphics.Canvas(800, 800, 'darkgreen');
+let canvas = canvasInstance.element;
+// gamid.graphics.setBackgroundColor(canvas, 'red');
+
+for (let i = 0; i < 100; i++){
+    // let abc=['blue', 'red', 'yellow', 'green', 'gray']
+    let xxx = Math.random()*800
+    let yyy = Math.random()*800
+
+    let color = 'red'
+
+    let abc=['#009','yellow','yellow']
+    if (xxx >400) {
+      abc=['white', 'black']
+    }
+    // FIXME DÁ PRA CRIAR UM OBJETO SQUARE SEM UMA DAS POSIÇÕES X E Y (CORRIJAM) (OU É PQ FICA SEM COR, SEI LÁ)
+    // let square = new gamid.graphics.Square(2, 2, Math.random()*800, Math.random()*800, abc[Math.floor(Math.random()*abc.length)]);
+
+    let square = new gamid.graphics.Square(30, 30, xxx, yyy, 1, abc[Math.floor(Math.random()*abc.length)]);
+    let squarecollision = new gamid.physics.SquareCollision(square);
+
+    a = new gamid.controller.Keyboard(square, [87, 65, 83, 68, 73, 74, 75, 76]);
+     // = new gamid.controller.Keyboard(square, [73, 74, 75, 76]);
+
+    canvasInstance.addObject(square);
+    // square.speedX = 20;
+    // square.speedY = 20;
+
+    // a.setOnKeyUp(87, function(){square.speedY = 0});
+    if (xxx<400) {
+      a.setOnKeyDown(87, function(){square.speedY -= Math.random()*100;});
+
+      // a.setOnKeyUp(83, function(){square.speedY = 0});
+      a.setOnKeyDown(83, function(){square.speedY += Math.random()*100; });
+
+      // a.setOnKeyUp(65, function(){square.speedX = 0});
+      a.setOnKeyDown(65, function(){square.speedX -= Math.random()*100; });
+
+      // a.setOnKeyUp(68, function(){square.speedX = 0});
+      a.setOnKeyDown(68, function(){square.speedX += Math.random()*100; });
+    } else {
+      a.setOnKeyDown(73, function(){square.speedY -= Math.random()*100;});
+
+      // a.setOnKeyUp(83, function(){square.speedY = 0});
+      a.setOnKeyDown(75, function(){square.speedY += Math.random()*100; });
+
+      // a.setOnKeyUp(65, function(){square.speedX = 0});
+      a.setOnKeyDown(74, function(){square.speedX -= Math.random()*100; });
+
+      // a.setOnKeyUp(68, function(){square.speedX = 0});
+      a.setOnKeyDown(76, function(){square.speedX += Math.random()*100; });
+  }
+
+}
+
+document.body.appendChild(canvas);
+
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
